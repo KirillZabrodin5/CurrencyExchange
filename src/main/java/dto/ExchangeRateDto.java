a7b2a752a28d5d373606c635f75a0ab30e9193de
KirillZabrodin5 <fea.z@yandex.ru>
2024-06-09 11:38:20 +0500

    delete files

--- src/main/java/dto/ExchangeRateDto.java
+++ src/main/java/dto/ExchangeRateDto.java
@@ -18,11 +18,6 @@ public class ExchangeRateDto {
     private Currency targetCurrency;
     private BigDecimal rate;
 
-    public ExchangeRateDto(Currency baseCurrency, Currency targetCurrency) {
-        this.baseCurrency = baseCurrency;
-        this.targetCurrency = targetCurrency;
-    }
-
     public ExchangeRateDto(Currency baseCurrency, Currency targetCurrency, BigDecimal rate) {
         this.baseCurrency = baseCurrency;
         this.targetCurrency = targetCurrency;
